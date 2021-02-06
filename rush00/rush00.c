d41173a1695d25c21d8da555d456131f420c5b23
Eldoctor26023 <lothvalentin@gmail.com>
2021-02-06 23:23:59 +0100

    rush 00 base

--- rush00/rush00.c
+++ rush00/rush00.c
@@ -2,7 +2,7 @@
 
 void	ft_putchar(char c)
 {
-	write(1, &c, 1);
+	write(1, &c,1);
 }
 void	condition(int abs, int ord, int x, int y)
 {
@@ -14,9 +14,21 @@ void	rush0(int x, int y)
 	int ord;
 	abs = 1;
 	ord = 1;
-	while (x > 0 && y > 0)
+	if  (x > 0 && y > 0)
 	{
-		condition(abs, ord, x, y);
+		while (ord <=y)
+		{
+			while (i <= x)
+			
+			{
+				condition(abs, ord, x, y);
+				abs++;
+			}
+		abs=1;
+		ft_putchar('\n');
+		j++;
+
+		}
 	}
 
 }
