c7ab881fbcfb670b3f972385a973a5c19da2ca07
Eldoctor26023 <lothvalentin@gmail.com>
2021-02-11 23:49:14 +0100

    a

---
+++ c02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int	condition(char c)
+{
+	if (c >= '0' && c <= '9')
+		return 1;
+	return 0;
+}
+int	ft_str_is_numeric(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i])
+	{
+		if (!(condition(str[i])))
+			return 0;
+		i++;
+	}
+	return 1;
+}
+int main()
+{
+	char tab[] = "\0";
+	printf("%d\n", ft_str_is_numeric(tab));
+	return 0;
+}
