f90acaa0ff278e6e6b37855f5f7352c2a81e31ac
Eldoctor26023 <lothvalentin@gmail.com>
2021-02-12 21:16:05 +0100

    a

---
+++ c02/ex06/ft_str_is_printable.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int	condition(char c)
+{
+	if (c >= 32 && c <= 126)
+		return 1;
+	return 0;
+}
+int	ft_str_is_printable(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i])
+	{
+		if (!(condition(str[i])))
+			return 0;
+		i++;
+	}
+	return 1;
+}
+int main ()
+{
+	char tab[]="\0";
+	printf("%d\n", ft_str_is_printable(tab));
+	return 0;
+}
